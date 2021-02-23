790c59af148e59c0ca807fba9086b729b65d97ea
Roman <61251937+RomanKlasnyy@users.noreply.github.com>
2021-02-23 11:29:51 +0200

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,84 @@
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import Http404, HttpResponse
+from datetime import datetime
+import json
+import random
+
+with open(settings.NEWS_JSON_PATH) as f:
+    articles = json.load(f)
+# Create your views here.
+
+
+class BaseView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'news/index.html'
+        )
+
+
+class NewsView(View):
+    def get(self, request, *args, **kwargs):
+        news_dict = {}
+        for article in articles:
+            date_value = article['created'].split(' ')[0]
+            if news_dict.get(date_value, 0) == 0:
+                news_dict[date_value] = [article]
+            else:
+                news_dict[date_value].append(article)
+
+        datelist = sorted({x['created'].split(" ")[0] for x in articles},
+                          reverse=True)
+        news_list = []
+        for _date in datelist:
+            date_news = {'created': _date, 'news': news_dict[_date]}
+            news_list.append(date_news)
+        search = request.GET.get('q')
+        if search:
+            searched_list = []
+            for date in news_list:
+                date_news = []
+                for new in date['news']:
+                    if search in new['title']:
+                        date_news.append(new)
+                if date_news:
+                    searched_date = {'created': date['created'], 'news': date_news}
+                    searched_list.append(searched_date)
+            news_list = searched_list
+
+        return render(request, "news/news.html", context={
+            'articles_dict': news_list
+        })
+
+
+class ArticleView(View):
+    def get(self, request, link_id, *args, **kwargs):
+        for article in articles:
+            if article['link'] == link_id:
+                return render(
+                    request, 'news/article.html',
+                    context=article)
+        else:
+            raise Http404
+
+
+class CreateView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'news/create.html')
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        all_links = {article["link"] for article in articles}
+        link = max(all_links) + 1
+        new_article = {'created': created, 'text': text,
+                       'title': title, 'link': link}
+
+        articles.append(new_article)
+
+        with open(settings.NEWS_JSON_PATH, 'w') as f:
+            json.dump(articles, f, indent=2)
+
+        return redirect('/news/')
